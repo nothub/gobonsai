92d9e192f13193c1631b8c8e7fcfa98773fe8ff3
John <20073178+jallbrit@users.noreply.github.com>
2020-06-03 13:36:41 -0500

    Add long option support

--- cbonsai.c
+++ cbonsai.c
@@ -1,11 +1,16 @@
 #include <stdlib.h>
 #include <ncurses.h>
 #include <unistd.h>
+#include <getopt.h>
+#include <time.h>
+#include <string.h>
 
 void finish() {
 	clear();
 	refresh();
 	endwin();
+
+	curs_set(1);	// make cursor visible again
 }
 
 void printHelp() {
@@ -37,18 +42,21 @@ void printHelp() {
 // attrset(A_NORMAL); || standend()
 
 int main(int argc, char* argv[]) {
-	int row = 0;
-	int col = 0;
-	int centerx, ch;
+	int rows = 0;
+	int cols = 0;
+	int y,x;
 
 	int live = 0;
 	int infinite = 0;
+	int screensaver = 0;
 
+	int verbosity = 0;
 	int termSize = 1;
 	int termColors = 0;
-	char leafStrs[] = "&";
 	int baseType = 1;
-	char message[] = "";
+	char *leafStrs = "&";
+	char *message;
+	char *geometry;
 
 	double multiplier = 5;
 	int lifeStart = 28;
@@ -57,21 +65,83 @@ int main(int argc, char* argv[]) {
 	double timeWait = 4;
 
 	int flag_m = 0;
-
 	int c;
 
+	struct option long_options[] = {
+		{"live", no_argument, NULL, 'l'},
+		{"time", required_argument, NULL, 't'},
+		{"infinite", no_argument, NULL, 'i'},
+		{"wait", required_argument, NULL, 'w'},
+		{"screensaver", no_argument, NULL, 'S'},
+		{"message", required_argument, NULL, 'm'},
+		{"termcolors", no_argument, NULL, 'T'},
+		{"geometry", required_argument, NULL, 'g'},
+		{"base", required_argument, NULL, 'b'},
+		{"leaf", required_argument, NULL, 'c'},
+		{"multiplier", required_argument, NULL, 'M'},
+		{"life", required_argument, NULL, 'L'},
+		{"seed", required_argument, NULL, 's'},
+		{"verbose", no_argument, NULL, 'v'},
+		{"help", no_argument, NULL, 'h'},
+		{0, 0, 0, 0}
+	};
+
+	// seed random number generator using time, and generate tree seed
+	srand(time(NULL));
+	int seed = rand();
+
 	// parse arguments
-	while ((c = getopt(argc, argv, "hlt:w:ig:c:Tm:b:M:L:s:vn")) != EOF) {
+	int option_index = 0;
+	while ((c = getopt_long(argc, argv, "lt:iw:Sm:Tg:b:c:M:L:s:vh", long_options, &option_index)) != -1) {
 		switch (c) {
-			case 'h':
-				printHelp();
-				return 0;
-				break;
 			case 'l':
 				live = 1;
 				break;
 			case 't':
 				timeStep = atof(optarg);
+				break;
+			case 'i':
+				infinite = 1;
+				break;
+			case 'w':
+				timeWait = atof(optarg);
+				break;
+			case 'S':
+				screensaver = 1;
+				break;
+			case 'm':
+				message = optarg;
+				break;
+			case 'T':
+				termColors = 1;
+				break;
+			case 'g':
+				geometry = optarg;
+				break;
+			case 'b':
+				baseType = atoi(optarg);
+				break;
+			case 'c':
+				leafStrs = optarg;
+				break;
+			case 'M':
+				multiplier = atoi(optarg);
+				break;
+			case 'L':
+				lifeStart = atoi(optarg);
+				break;
+			case 's':
+				seed = atoi(optarg);
+				break;
+			case 'v':
+				verbosity++;
+				break;
+
+			// '?' represents unknown option. Treat it like --help.
+			case '?':
+			case 'h':
+				printHelp();
+				return 0;
 				break;
 		}
 	}
@@ -79,6 +149,7 @@ int main(int argc, char* argv[]) {
 	initscr();	// init ncurses screen
 	savetty();
 	noecho();	// don't echo input to screen
+	curs_set(0);	// make cursor invisible
 	cbreak();	// don't wait for new line to grab user input
 
 	// if terminal has color capabilities, use them
